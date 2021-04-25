17bd2bd74a184f2287362354ba14e548709c29b1
Daphaz <bonnet.damien@ymail.com>
2021-04-26 00:05:24 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/global.css";
+import { AuthProvider } from "../auth/context";
+import Nprogress from "nprogress";
+import Router from "next/router";
+import Head from "next/head";
+import { Layout } from "../components";
+
+Router.events.on("routeChangeStart", () => {
+	Nprogress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+	Nprogress.done();
+});
+Router.events.on("routeChangeError", () => {
+	Nprogress.start();
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Layout>
+			<Head>
+				<link rel="stylesheet" type="text/css" href="/nprogress.css" />
+			</Head>
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+		</Layout>
+	);
 }
 
-export default MyApp
+export default MyApp;
