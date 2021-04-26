45602929430abfc95062ac92351a6e965cd2e6b7
Daphaz <bonnet.damien@ymail.com>
2021-04-26 02:01:16 +0200

    feat(footer): add structure css and component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,117 @@
 import React from "react";
 
 const Footer = () => {
-	return <footer className="footer">Footer</footer>;
+	return (
+		<footer className="footer">
+			<div className="container">
+				<div className="footer--top">
+					<div className="footer--item">
+						<div className="footer--headline">ipsum dolor.</div>
+						<ul className="footer--links">
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+						</ul>
+					</div>
+					<div className="footer--item">
+						<div className="footer--headline">ipsum dolor.</div>
+						<ul className="footer--links">
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+						</ul>
+					</div>
+					<div className="footer--item">
+						<div className="footer--headline">ipsum dolor.</div>
+						<ul className="footer--links">
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+						</ul>
+					</div>
+					<div className="footer--item">
+						<div className="footer--headline">ipsum dolor.</div>
+						<ul className="footer--links">
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+							<li className="footer--link">
+								<a href="#">Lorem</a>
+							</li>
+						</ul>
+					</div>
+					<div className="footer--subscribe">
+						<div className="footer--headline">ipsum dolor.</div>
+						<div className="footer--subtitle">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit
+								aliquid repellat repudiandae ratione voluptate quidem provident.
+							</p>
+						</div>
+						<form className="form--subscribe">
+							<div className="form--line">
+								<input type="email" name="email" id="email" />
+								<button type="submit">
+									<div className="icon--send"></div>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div className="footer--bottom">
+					<div className="footer--bottom_left">
+						<p>Lorem ipsum dolor sit amet consectetur.</p>
+					</div>
+					<div className="footer--bottom_right">
+						<ul>
+							<li>
+								<a href="#">Lorem, ipsum.</a>
+							</li>
+							<li>
+								<a href="#">Lorem, ipsum.</a>
+							</li>
+							<li>
+								<a href="#">Lorem, ipsum.</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
