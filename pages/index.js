32cac49bb56d4a227a7cd25e34eb885a1c2eb620
Daphaz <bonnet.damien@ymail.com>
2021-04-27 23:58:34 +0200

    feat(layout): add layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from "next/head";
+import { Layout } from "../components";
 
 export default function Home() {
 	return (
-		<>
+		<Layout>
 			<Head>
 				<title>E-shop Project</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta name="Description" content="E-shop node and nextjs" />
 			</Head>
 			<div className="container">hello</div>
-		</>
+		</Layout>
 	);
 }
