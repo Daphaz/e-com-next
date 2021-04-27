0a089d05c3e3ffbef408554c44d622306ed2fb63
Daphaz <bonnet.damien@ymail.com>
2021-04-28 01:07:06 +0200

    feat(dashboard): add widget orders

--- pages/gestion/index.jsx
+++ pages/gestion/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "../../components/Admin/Layout";
 import ListWidgets from "../../components/Admin/ListWidgets";
+import Orders from "../../components/Admin/Orders";
+import styles from "../../styles/admin.module.scss";
 
 const Dashboard = () => {
 	return (
 		<Layout>
 			<ListWidgets />
-			<div className="dashboard__content">Content</div>
+			<div className={styles.admin__content}>
+				<Orders />
+			</div>
 		</Layout>
 	);
 };
