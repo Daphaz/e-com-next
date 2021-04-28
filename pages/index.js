35f7c9ed6670ebbdb851f298b6482f74e67f1f84
Daphaz <bonnet.damien@ymail.com>
2021-04-28 03:01:07 +0200

    fix: global  meta in _app

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ export default function Home() {
 		<Layout>
 			<Head>
 				<title>E-shop Project</title>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<meta name="Description" content="E-shop node and nextjs" />
 			</Head>
 			<div className="container">hello</div>
 		</Layout>
