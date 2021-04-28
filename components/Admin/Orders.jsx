1a11610e45d12f91d9e8a5fd5a055566a5ed1f73
Daphaz <bonnet.damien@ymail.com>
2021-04-28 23:38:24 +0200

    feat(orders): add header with button for create a new order

--- components/Admin/Orders.jsx
+++ components/Admin/Orders.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "../../styles/admin.module.scss";
+import AddOrder from "./../../public/icons/icon-document-add.svg";
 
-const Orders = () => {
+const Orders = ({ commandes }) => {
 	return (
-		<div className={styles.admin__orders}>
-			<h2>Commandes récentes</h2>
+		<div className={styles.admin__table}>
+			{!commandes ? (
+				<h2>Commandes récentes</h2>
+			) : (
+				<div className={styles.table__header}>
+					<h3>Commandes</h3>
+					<div className={styles.table__create}>
+						<button className="btn btn__create">
+							<AddOrder />
+							Créer une commande
+						</button>
+					</div>
+				</div>
+			)}
 			<div className={styles.table}>
 				<div className={`${styles.table__row} ${styles.table__head}`}>
 					<div className={styles.table__cell}>Client</div>
