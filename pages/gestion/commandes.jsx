5778947e1e9882154f1046603618107ae1e3b924
Daphaz <bonnet.damien@ymail.com>
2021-04-28 23:41:24 +0200

    feat(commandes): create new page and some css rules

---
+++ pages/gestion/commandes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Admin/Layout";
+import styles from "../../styles/admin.module.scss";
+import Orders from "../../components/Admin/Orders";
+
+const Commandes = () => {
+	return (
+		<Layout>
+			<div className={styles.admin__content}>
+				<Orders commandes />
+			</div>
+		</Layout>
+	);
+};
+
+export default Commandes;
