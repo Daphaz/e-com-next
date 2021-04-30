e646c074a0309606a2f9697693ec1be2d09d5349
Daphaz <bonnet.damien@ymail.com>
2021-04-30 23:31:08 +0200

    feat(dashboard): change index for login

--- pages/gestion/index.jsx
+++ pages/gestion/index.jsx
@@ -1,18 +1,42 @@
 import React from "react";
-import Layout from "../../components/Admin/Layout";
-import ListWidgets from "../../components/Admin/ListWidgets";
-import Orders from "../../components/Admin/Orders";
 import styles from "../../styles/admin.module.scss";
 
-const Dashboard = () => {
+const Login = () => {
 	return (
-		<Layout>
-			<ListWidgets />
-			<div className={styles.admin__content}>
-				<Orders />
+		<div className={styles.loginAdmin}>
+			<div className={styles.loginAdmin__left}>
+				<img src="/images/loginAdmin.jpg" alt="login page" />
 			</div>
-		</Layout>
+			<div className={styles.loginAdmin__right}>
+				<h3>Gestion de E-shop</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<form className="form">
+					<div className="form__group">
+						<label htmlFor="email">Email</label>
+						<input type="email" name="email" id="email" />
+						<div className="alert alert__danger">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+							itaque.
+						</div>
+					</div>
+					<div className="form__group">
+						<label htmlFor="password">Password</label>
+						<input type="password" name="password" id="password" />
+						<div className="alert alert__danger">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+							itaque.
+						</div>
+					</div>
+					<button type="submit" className="btn btn__primary">
+						login
+					</button>
+					<a href="#" className="form__link">
+						Mot de passe oublié ?
+					</a>
+				</form>
+			</div>
+		</div>
 	);
 };
 
-export default Dashboard;
+export default Login;
