cbff038b617e80875af2f08623bbe804199feb93
Daphaz <bonnet.damien@ymail.com>
2021-05-03 22:53:03 +0200

    feat(product): change swr by useEffect for reload after deleting

--- pages/gestion/produits/index.jsx
+++ pages/gestion/produits/index.jsx
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../../components/Admin/Layout";
 import styles from "../../../styles/admin.module.scss";
 import ProduitsTable from "../../../components/Admin/ProduitsTable";
 import { ProtectedRouteAdmin } from "../../../auth/protectedRoutes";
 import useAuth from "../../../auth/context";
 import api from "../../../auth/axios";
-import useSWR from "swr";
-
-const fetcher = (url) => api.get(url).then((res) => res.data.data);
 
 const Produits = () => {
+	const [products, setProducts] = useState(null);
 	const { isAuthenticatedAdmin } = useAuth();
 
-	const { data: products } = useSWR("/product/all", fetcher);
+	async function loadProducts() {
+		setProducts(null);
+		try {
+			const { data } = await api.get("/product/all");
+			if (data.status) {
+				setProducts(data.data);
+			} else {
+				setProducts([]);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		loadProducts();
+	}, []);
+
+	async function deleteProduct(id) {
+		try {
+			const { data } = await api.delete("/product/delete", {
+				data: { id },
+			});
+
+			if (data.status) {
+				loadProducts();
+			} else {
+				console.log(data.message);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<>
 			{isAuthenticatedAdmin && (
 				<Layout>
 					<div className={styles.admin__content}>
-						{products && <ProduitsTable products={products} />}
+						{products && (
+							<ProduitsTable
+								products={products}
+								deleteProduct={deleteProduct}
+							/>
+						)}
 					</div>
 				</Layout>
 			)}
