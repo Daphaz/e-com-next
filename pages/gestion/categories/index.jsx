f0d7d2fcd36817c7f9c6ee9e5f7e7db071ee5a52
Daphaz <bonnet.damien@ymail.com>
2021-05-04 00:35:14 +0200

    feat: add delete function and change swr to useEffect

--- pages/gestion/categories/index.jsx
+++ pages/gestion/categories/index.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../../components/Admin/Layout";
 import styles from "../../../styles/admin.module.scss";
 import CategorieTable from "../../../components/Admin/CategorieTable";
 import { ProtectedRouteAdmin } from "../../../auth/protectedRoutes";
 import useAuth from "../../../auth/context";
 import api from "../../../auth/axios";
-import useSWR from "swr";
 
 const fetcher = (url) =>
 	api
@@ -13,16 +12,54 @@ const fetcher = (url) =>
 		.then((res) => (res.data.data === undefined ? [] : res.data.data));
 
 const Categories = () => {
+	const [categories, setCategories] = useState(null);
 	const { isAuthenticatedAdmin } = useAuth();
 
-	const { data: categories } = useSWR("/category/all", fetcher);
+	async function loadCategories() {
+		setCategories(null);
+		try {
+			const { data } = await api.get("/category/all");
+			if (data.status) {
+				setCategories(data.data);
+			} else {
+				setCategories([]);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		loadCategories();
+	}, []);
+
+	async function deleteCategory(id) {
+		try {
+			const { data } = await api.delete("/category/delete", {
+				data: { id },
+			});
+
+			if (data.status) {
+				loadCategories();
+			} else {
+				console.log(data.message);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<>
 			{isAuthenticatedAdmin && (
 				<Layout>
 					<div className={styles.admin__content}>
-						{categories && <CategorieTable categories={categories} />}
+						{categories && (
+							<CategorieTable
+								categories={categories}
+								deleteCategory={deleteCategory}
+							/>
+						)}
 					</div>
 				</Layout>
 			)}
