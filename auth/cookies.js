df9343f5d17b967d4a7fec7cd269784631f6618f
Daphaz <bonnet.damien@ymail.com>
2021-05-06 17:06:05 +0200

    feat(auht): change cookie expire for dev

--- auth/cookies.js
+++ auth/cookies.js
@@ -3,7 +3,7 @@ import cookie from "js-cookie";
 export const setCookie = (key, value) => {
 	if (process.browser) {
 		cookie.set(key, value, {
-			expires: 5,
+			expires: 1,
 			path: "/",
 			sameSite: "Strict",
 		});
