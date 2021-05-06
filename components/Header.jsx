81e73186b0344269d49331cc66b1b7604befb8d3
Daphaz <bonnet.damien@ymail.com>
2021-05-07 00:08:57 +0200

    feat: add header mobile view

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,43 +1,84 @@
-import React from "react";
+import React, { useRef } from "react";
+import useAuth from "../auth/context";
+import IconUser from "../public/icons/icon-user.svg";
+import IconShop from "../public/icons/icon-shopping-cart.svg";
+import IconMenu from "../public/icons/icon-menu.svg";
+import IconClose from "../public/icons/icon-close.svg";
 
 const Header = () => {
+	const menu = useRef(null);
+	const { logout, user } = useAuth();
+
+	function openMenu() {
+		menu.current.classList.toggle("active");
+		document.body.classList.toggle("is_open");
+	}
 	return (
-		<header className="header">
-			<div className="container">
-				<div className="header--logo">Logo</div>
-				<nav className="header--nav">
-					<ul className="header--navlinks">
-						<li className="header--links">
-							<a href="#">lorem</a>
-						</li>
-						<li className="header--links">
-							<a href="#">lorem</a>
-						</li>
-						<li className="header--links">
-							<a href="#">lorem</a>
-						</li>
-					</ul>
-				</nav>
-				<div className="header--actions">
-					<a href="#">
-						<img
-							src="/icons/icon-user.svg"
-							alt="user"
-							width="30px"
-							height="30px"
-						/>
-					</a>
-					<a href="#">
-						<img
-							src="/icons/icon-shopping-cart.svg"
-							alt="cart"
-							width="30px"
-							height="30px"
-						/>
-					</a>
+		<>
+			<div className="header__menuMobile_bg" ref={menu}>
+				<div className="header__menuMobile">
+					<div className="header__menuMobile_close" onClick={openMenu}>
+						<IconClose />
+					</div>
+					<nav className="header__nav">
+						<ul className="header__navlinks">
+							<li className="header__links">
+								<a href="#">Compte</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Nos produits</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Qui sommes nous ?</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</nav>
+					<div className="header__actions">
+						<a href="#">
+							<IconUser />
+						</a>
+						<a href="#">
+							<IconShop />
+						</a>
+					</div>
 				</div>
 			</div>
-		</header>
+			<header className="header">
+				<div className="container">
+					<div className="header__logo">E-Shop</div>
+					<nav className="header__nav">
+						<ul className="header__navlinks">
+							<li className="header__links">
+								<a href="#">Compte</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Nos produits</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Qui sommes nous ?</a>
+							</li>
+							<li className="header__links">
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</nav>
+					<div className="header__actions">
+						<a href="#">
+							<IconUser />
+						</a>
+						<a href="#">
+							<IconShop />
+						</a>
+					</div>
+					<div className="header__burger" onClick={openMenu}>
+						<IconMenu />
+					</div>
+				</div>
+			</header>
+		</>
 	);
 };
 
